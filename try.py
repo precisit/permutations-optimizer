acb36cf48261194ac61acd3ae68b3c23517e7bcc
Martin Stålberg <mast4461@gmail.com>
2016-08-11 00:47:08 +0200

    More people. Plotting.

--- try.py
+++ try.py
@@ -1,5 +1,7 @@
 import random
 import math
+from matplotlib import pyplot as plt
+
 
 names = [
 	"Magnus",
@@ -7,7 +9,11 @@ names = [
 	"Martin",
 	"Caroline",
 	"Emma",
-	"Samuel"
+	"Samuel",
+	"Viktor",
+	"Jacob",
+	"Jonas",
+	"Sigrid"
 ]
 
 	#Magnus
@@ -16,22 +22,49 @@ names = [
 				#Caroline
 					#Emma
 						#Samuel
+							#Viktor
+								#Jacob
+									#Jonas
+										#Sigrid
 personPairScore = [
-	[0,	3,	3,	1,	1,	1], #Magnus
-	[3,	0,	3,	1,	1,	1], #Nils
-	[3,	3,	0,	1,	1,	1], #Martin
-	[1,	1,	1,	0,	1,	1], #Caroline
-	[1,	1,	1,	1,	0,	1], #Emma
-	[1,	1,	1,	1,	1,	0], #Samuel
+	[0,	3,	3,	1,	1,	1,	2,	2,	2,	3], #Magnus
+	[3,	0,	3,	1,	1,	1,	2,	2,	2,	3], #Nils
+	[3,	3,	0,	1,	1,	1,	2,	2,	2,	3], #Martin
+	[1,	1,	1,	0,	1,	1,	1,	1,	1,	2], #Caroline
+	[1,	1,	1,	1,	0,	1,	1,	1,	1,	2], #Emma
+	[1,	1,	1,	1,	1,	0,	1,	1,	1,	1], #Samuel
+	[1,	1,	1,	1,	1,	0,	0,	3,	3,	2], #Viktor
+	[1,	1,	1,	1,	1,	0,	3,	0,	3,	2], #Jacob
+	[1,	1,	1,	1,	1,	0,	3,	3,	0,	2], #Jonas
+	[3,	3,	3,	2,	2,	1,	2,	2,	2,	0] #Sigrid
 ]
 
+# One table of six and one table of four
+# seats = [
+# 	[0, 0],
+# 	[1, 0],
+# 	[2, 0],
+# 	[0, 1],
+# 	[1, 1],
+# 	[2, 1],
+# 	[6, 0],
+# 	[7, 0],
+# 	[6, 1],
+# 	[7, 1],
+# ]
+
+# One table of ten
 seats = [
 	[0, 0],
 	[1, 0],
 	[2, 0],
+	[3, 0],
+	[4, 0],
 	[0, 1],
 	[1, 1],
-	[2, 1]
+	[2, 1],
+	[3, 1],
+	[4, 1],
 ]
 
 def getSeatPairScore(seats):
@@ -83,16 +116,21 @@ if __name__ == '__main__':
 	# personOrder = [0,3,1,4,2,5]
 
 	temperature = sum([sum(s) for s in personPairScore])
-	alpha = 0.99
+	alpha = 0.999
 
 	bestCost = float("inf")
 	bestPersonOrder = []
 
-	for r in range(500):
+	iAll = range(10000)
+	costs = list(iAll)
+
+	for i in iAll:
 		newPersonOrder = getNewPersonOrder(personOrder)
 
 		cost1 = objectiveFunction(personOrder, personPairScore, seatPairScore)
 		cost2 = objectiveFunction(newPersonOrder, personPairScore, seatPairScore)
+
+		costs[i] = cost1
 
 		costDelta = cost2 - cost1
 
@@ -114,3 +152,6 @@ if __name__ == '__main__':
 
 	print "**************"
 	print bestCost, [names[i] for i in personOrder]
+
+	plt.plot(iAll, costs)
+	plt.show()
