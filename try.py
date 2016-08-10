db032ce9425e57aa467eab74c1c05ab4739c5430
Martin Stålberg <mast4461@gmail.com>
2016-08-10 23:17:58 +0200

    ?

--- try.py
+++ try.py
@@ -1,26 +1,44 @@
-persons = [
-	{
-		"name": "A"
-	},
-	{
-		"name": "B"
-	}
+names = [
+	"Alice",
+	"Bob",
+	"Chloe",
+	"Daniel"
 ]
 
-seats = {
-	1: {
-		"person": 0,
-		"neighboringSeats": [2]
-	},
-	2: {
-		"person": 0,
-		"neighboringSeats": [1, 3]
-	},
-	3: {
-		"person": 0
-		"neighboringSeats": [2]
-	}
-}
+	#Alice
+		#Bob
+			#Chloe
+				#Daniel
+pairScore = [
+	[0,	1,	2,	1], #Alice
+	[1, 0,	1,	2], #Bob
+	[2,	1,	0,	1], #Chloe
+	[1,	2,	1,	0] #Daniel
+]
+
+seats = [
+	[0, 0],
+	[1, 0],
+	[1, 1],
+	[2, 0]
+]
+
+def getSeatNeighbors(seats):
+	seatNeighborIndexes = [[] for seat in seats]
+
+	for i1, seat1 in enumerate(seats):
+		x1 = seat1[0]
+		y1 = seat1[1]
+
+		for i2, seat2 in enumerate(seats):
+			x2 = seat2[0]
+			y2 = seat2[1]
+
+			delta = (x2-x1)**2 + (y2-y1)**2
+
+			print seat1, seat2, delta
+
+
 
 def getPerson(seat):
 	pass
@@ -42,5 +60,5 @@ def assignSeats(persons, seats):
 
 
 if __name__ == '__main__':
-	assignSeats(persons, seats)
-	print seats
+	getSeatNeighbors(seats)
+	# print seats
